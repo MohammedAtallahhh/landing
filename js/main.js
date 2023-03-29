f8c3cee197fb6da267935cdbd1b32c0fa086b3ce
Mohammed Atallah <mohamedatallahh2017@gmail.com>
2023-03-30 01:29:49 +0200

    Used tailwindcss CLI instead of CDN

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const menuIcon = document.querySelector(".header .menu-icon");
 const mobileNav = document.querySelector(".mobile-nav");
 const closeButton = document.querySelector(".mobile-nav .close");
+const aboutSliderEl = document.querySelector("#about-us-slider");
 
 menuIcon.addEventListener("click", () => {
   mobileNav.classList.add("translate-x-0");
@@ -8,4 +9,12 @@ menuIcon.addEventListener("click", () => {
 
 closeButton.addEventListener("click", () => {
   mobileNav.classList.remove("translate-x-0");
+});
+
+// Sliders
+const aboutSlider = new Swiper("#about-us-slider", {
+  pagination: {
+    el: ".about-us-slider-controls",
+    clickable: true,
+  },
 });
