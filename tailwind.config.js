a6870f569e2cbe877e1ea59b913f0de53cc7a44f
Mohammed Atallah <mohamedatallahh2017@gmail.com>
2023-03-30 02:39:31 +0200

    added services section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "3xl": "0px 4px 30px rgba(0, 0, 0, 0.1);",
+      },
+    },
   },
   plugins: [],
 };
