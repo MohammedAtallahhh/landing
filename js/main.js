291ba55f0337d1c19052fe491d5fc38d21b49970
Mohammed Atallah <mohamedatallahh2017@gmail.com>
2023-03-30 17:49:53 +0200

    Added achievements section

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const menuIcon = document.querySelector(".header .menu-icon");
 const mobileNav = document.querySelector(".mobile-nav");
 const closeButton = document.querySelector(".mobile-nav .close");
 const aboutSliderEl = document.querySelector("#about-us-slider");
+const countElements = document.querySelectorAll(".achievements .count");
 
 menuIcon.addEventListener("click", () => {
   mobileNav.style.transform = "translateX(0)";
@@ -34,4 +35,45 @@ const howSlider = new Swiper("#how-slider", {
     el: ".how-slider-controls",
     clickable: true,
   },
+});
+
+const threeDSlider = new Swiper("#three-d-slider", {
+  autoplay: {
+    delay: 8000,
+  },
+
+  navigation: {
+    nextEl: "#three-d-slider .slider-buttons .next",
+    prevEl: "#three-d-slider .slider-buttons .prev",
+  },
+});
+
+// Animate count elements on scroll
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const countEl = entry.target;
+      const targetCount = +countEl.getAttribute("data-count");
+      const increment = Math.ceil(targetCount / 100);
+      let currentCount = 0;
+
+      const updateCount = () => {
+        if (currentCount < targetCount) {
+          currentCount += increment;
+          countEl.innerText = currentCount;
+          setTimeout(updateCount, 10);
+        } else {
+          countEl.innerText = targetCount;
+        }
+      };
+
+      updateCount();
+
+      observer.unobserve(countEl);
+    }
+  });
+});
+
+countElements.forEach((countEl) => {
+  observer.observe(countEl);
 });
